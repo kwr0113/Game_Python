84ce5835b0a6a0120f075bda8d4f49e0592ecd66
kwr0113 <kwr0113@gmail.com>
2021-08-19 22:43:29 +0900

    chap8 ~ chap10

---
+++ chap 8/list0803_1.py
@@ -0,0 +1,23 @@
+import tkinter
+
+
+key = 0
+
+
+def key_down(e):
+    global key
+    key = e.keycode
+    print("KEY:" + str(key))
+
+
+def main_proc():
+    label['text'] = key
+    root.after(100, main_proc)
+
+root = tkinter.Tk()
+root.title("실시간 키 입력")
+root.bind("<KeyPress>", key_down)
+label = tkinter.Label(font=("Times New Roman", 80))
+label.pack()
+main_proc()
+root.mainloop()
